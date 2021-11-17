98fbac5b323f56794092f317065c2f253c4682c1
Katyaryna <79213670+Katyaryna@users.noreply.github.com>
2021-11-17 18:15:24 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/*Динамическое изменение содержимого <p>*/
+let something="Katya_Kamenko";
+document.getElementsByTagName("p")[0].innerHTML=something;
+
+
+document.getElementsByClassName("warning")[0].innerHTML="Что-то для класса warning";
+
